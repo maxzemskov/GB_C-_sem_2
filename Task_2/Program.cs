e76789284facf52efdecd6ff0d6388b20dc7073d
mzemskov <mzemskov@dimax.digital>
2022-11-25 00:29:25 +0300

    task_2+stoppers_task_1

---
+++ Task_2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine());
+
+if (a < 100)
+	Console.WriteLine("Третья цифра отсутствует");
+else
+{	
+	while (a >= 1000)
+	{
+		a = a / 10;
+	}	
+int b = a % 10;
+Console.WriteLine($"Число с индексом 2 - {b}");	
+}	
