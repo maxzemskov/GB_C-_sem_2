324842761b8f9d3e3f068e28739f9f3cfd49e702
mzemskov <mzemskov@dimax.digital>
2022-11-24 23:18:57 +0300

    task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,11 @@
+﻿
+Console.Clear();
+Console.WriteLine("Введите трехзначное число");
+		int a = int.Parse(Console.ReadLine());
+	
+		int b = a / 10;
+		int c = b % 10;
+
+		Console.WriteLine($"число под индексом 1 - {c}");
+
+			
