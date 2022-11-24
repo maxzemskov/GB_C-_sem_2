4e0b9c2d8a37425236a4626c65d0d0a0257f06a4
mzemskov <mzemskov@dimax.digital>
2022-11-25 00:30:02 +0300

    task_1_stopper

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -2,10 +2,13 @@
 Console.Clear();
 Console.WriteLine("Введите трехзначное число");
 		int a = int.Parse(Console.ReadLine());
-	
-		int b = a / 10;
-		int c = b % 10;
-
-		Console.WriteLine($"число под индексом 1 - {c}");
+	if (a < 1000 && a > 100)
+		{
+			int b = a / 10;
+			int c = b % 10;
+			Console.WriteLine($"число под индексом 1 - {c}");
+		}
+	else
+		Console.WriteLine("введено не 3х значное число");
 
 			
