58350ad84e1ac1e93fb8747b059830a1e81b0754
mzemskov <mzemskov@dimax.digital>
2022-11-25 00:42:18 +0300

    task_3

---
+++ Task_3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите день недели");
+int a = int.Parse(Console.ReadLine());
+
+
+if (a >=6 && a <= 7)
+	Console.WriteLine("Выходной день");
+if (a >= 1 && a <= 5)
+	Console.WriteLine("Рабочий день");
+if (a >=8 || a <= 0)
+Console.WriteLine("Такой день отсутствует в 7ми дневной неделе");
