1e20a81d57a67981e612fa2ea0bca06770f0295f
mzemskov <mzemskov@dimax.digital>
2022-11-26 20:33:28 +0300

    added space

--- Task_3/Program.cs
+++ Task_3/Program.cs
@@ -8,5 +8,5 @@ if (a >=6 && a <= 7)
 	Console.WriteLine("Выходной день");
 if (a >= 1 && a <= 5)
 	Console.WriteLine("Рабочий день");
-if (a >=8 || a <= 0)
+if (a >= 8 || a <= 0)
 Console.WriteLine("Такой день отсутствует в 7ми дневной неделе");
